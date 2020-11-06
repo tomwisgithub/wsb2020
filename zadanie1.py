50d802830c5392fe376b79473420b462564ee818
tomwisgithub <tomwismail@gmail.com>
2020-11-06 23:54:14 +0100

    Zadania z WSB

--- zadanie1.py
+++ zadanie1.py
@@ -1,68 +1,16 @@
 #!/usr/bin/env python3
 
-###zadanie 1
-###zadanie 2
+###dane = int(input("Podaj liczby po przecinku: "))
 
-###zadanie 3 [done]
+li = []
 
-#import math
+cudz = ("10")
 
-#r = int(input("Podaj promień koła: "))
+newIteams = input("Wpisz dane do listy: oddzielając je cudzysłowiem: ")
 
-#p = math.pi * r**2
+li.append(newIteams)
 
-#print("Powierzchnia koła wynosi: ", p)
+krotka = (newIteams)
 
-###zadanie 4 [done]
-
-#import math
-
-#silnia = 8
-#silniaDlaOsiem = math.factorial(silnia)
-#print(silniaDlaOsiem - 40320)
-
-###zadanie 5 [done]
-
-#Kolory = ['czerwony', 'zielony', 'niebieski', 'czarny']
-
-#print(Kolory[0::3])
-
-###zadanie 6 [done?]
-# Napisz program zliczający numer 4 dla podanej listy
-
-#Kolory = ['czerwony', 'zielony', 'niebieski', 'czarny']
-
-#print(Kolory[3])
-
-###zadanie 7
-###zadanie 8 [done]
-#x = 4
-#y = 3 
-#zadanie = (x + y) ** 2
-#print(zadanie)
-
-###zadanie 9 [done]
-
-#cal = int(input("Podaj wartość w calach: "))
-#stopa = int(input("Podaj wartość w stopach: "))
-#
-#calToCm = cal * 2.54
-#stopaToCm = stopa * 30.48
-#
-#print("Podana ilość cali wynosi ", calToCm, "centymetrów")
-#print("Podana ilość stóp wynosi ", stopaToCm, "centymetrów")
-
-###zadanie 10 [done]
-
-#sex = input("Czy chcesz obliczyć BMI dla kobiety czy dla mężczyzny?: (k/m) ")
-
-#if sex == "k":
-#    wagaKobieta = int(input("Podaj swoją wagę: "))
-#    wzrostKobieta = int(input("Podaj swój wzrost: "))
-#    print("Twoje BMI wynosi: {:.2f}".format(wagaKobieta/(wzrostKobieta/100)**2))
-#else:
-#    wagaMezczyzny = int(input("Podaj swoją wagę: "))
-#    wzrostMezczyzny = int(input("Podaj swój wzrost: "))
-#    print("Twoje BMI wynosi: {:.2f}".format(wagaMezczyzny/(wzrostMezczyzny/100)**2))
-
-###zadanie 11
+print(li)
+print(krotka)
---
+++ zadanie7.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+A = np.array([[-1], [7], [-26]])
