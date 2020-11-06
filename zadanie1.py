42e2045caeb845caf5968dd172dfef17b0744812
tomwisgithub <tomwismail@gmail.com>
2020-11-06 22:10:36 +0100

    master

---
+++ zadanie1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+###zadanie 1
+###zadanie 2
+
+###zadanie 3 [done]
+
+#import math
+
+#r = int(input("Podaj promień koła: "))
+
+#p = math.pi * r**2
+
+#print("Powierzchnia koła wynosi: ", p)
+
+###zadanie 4 [done]
+
+#import math
+
+#silnia = 8
+#silniaDlaOsiem = math.factorial(silnia)
+#print(silniaDlaOsiem - 40320)
+
+###zadanie 5 [done]
+
+#Kolory = ['czerwony', 'zielony', 'niebieski', 'czarny']
+
+#print(Kolory[0::3])
+
+###zadanie 6 [done?]
+# Napisz program zliczający numer 4 dla podanej listy
+
+#Kolory = ['czerwony', 'zielony', 'niebieski', 'czarny']
+
+#print(Kolory[3])
+
+###zadanie 7
+###zadanie 8 [done]
+#x = 4
+#y = 3 
+#zadanie = (x + y) ** 2
+#print(zadanie)
+
+###zadanie 9 [done]
+
+#cal = int(input("Podaj wartość w calach: "))
+#stopa = int(input("Podaj wartość w stopach: "))
+#
+#calToCm = cal * 2.54
+#stopaToCm = stopa * 30.48
+#
+#print("Podana ilość cali wynosi ", calToCm, "centymetrów")
+#print("Podana ilość stóp wynosi ", stopaToCm, "centymetrów")
+
+###zadanie 10 [done]
+
+#sex = input("Czy chcesz obliczyć BMI dla kobiety czy dla mężczyzny?: (k/m) ")
+
+#if sex == "k":
+#    wagaKobieta = int(input("Podaj swoją wagę: "))
+#    wzrostKobieta = int(input("Podaj swój wzrost: "))
+#    print("Twoje BMI wynosi: {:.2f}".format(wagaKobieta/(wzrostKobieta/100)**2))
+#else:
+#    wagaMezczyzny = int(input("Podaj swoją wagę: "))
+#    wzrostMezczyzny = int(input("Podaj swój wzrost: "))
+#    print("Twoje BMI wynosi: {:.2f}".format(wagaMezczyzny/(wzrostMezczyzny/100)**2))
+
+###zadanie 11
